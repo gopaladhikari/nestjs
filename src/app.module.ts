95a9f151131e6f05a0766405b0aa0990f7ad9a30
gopaladhikari <gopuadks@gmail.com>
2024-12-20 19:47:01 +0545

    employee resouce complted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
+import { DatabaseModule } from "./database/database.module";
+import { EmployeesModule } from "./employees/employees.module";
 
 @Module({
-	imports: [UsersModule],
+	imports: [DatabaseModule, EmployeesModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
---
+++ src/database/database.service.ts
@@ -0,0 +1,12 @@
+import { Injectable, type OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class DatabaseService
+	extends PrismaClient
+	implements OnModuleInit
+{
+	async onModuleInit() {
+		await this.$connect();
+	}
+}
